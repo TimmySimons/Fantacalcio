e192c6fd692c1a030c456067cbbb5c231cf43556
Hanne Robben <hannerobben@hotmail.com>
2025-08-25 10:42:43 +0200

    Fetch sorare from vercel

--- src/sorare/sorare.api.ts
+++ src/sorare/sorare.api.ts
@@ -2,7 +2,9 @@ export class SorareApi {
     public static async getPlayerStats(slug: string) {
         console.log('Vercel: getVercelPlayStats');
 
-        return await fetch(`/api/player?slug=${slug}`)
+        const vercelPath = import.meta.env.VITE_VERCEL_URL;
+
+        return await fetch(`${vercelPath}/api/player?slug=${slug}`)
             .then((res) => res.json())
             .then((data) => {
                 console.log('Sorare:', data);
