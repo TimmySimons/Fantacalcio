e46c4a111bae35849d4566bd48a4834641aae0f3
Hanne Robben <hannerobben@hotmail.com>
2025-08-25 11:14:56 +0200

    Handle vercel CORS

--- api/player.js
+++ api/player.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://timmysimons.github.io/Fantacalcio');
+    res.setHeader('Access-Control-Allow-Origin', 'https://timmysimons.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
